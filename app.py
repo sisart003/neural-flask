2647e2223a17e32ba1d5d85acb8172b46f912e68
Sisart003 <chrishart.estrada@gmail.com>
2025-03-21 05:47:41 +0800

    HTML Templating, Redirects

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
-from flask import Flask, request, make_response
+from flask import Flask, render_template, redirect, url_for
+# request, make_response
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return 'Hello, Chrishart!'
+    myvalue = 'Chrishart E. Estrada'
+    myresult = 10 + 20
+    mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    return render_template('index.html', value=myvalue, result=myresult, mylist=mylist)
+
+@app.route('/nextpage')
+def nextpage():
+    some_text = 'Hello, World!'
+    return render_template('nextpage.html', some_text=some_text)
+
+@app.template_filter('reverse_string')
+def reverse_string(s):
+    return s[::-1]
+
+@app.template_filter('repeat_string')
+def repeat_string(s, n=2):
+    return s * n
+
+
+@app.template_filter('alternate_case')
+def alternate_case(s):
+    return ''.join(c.upper() if i % 2 == 0 else c.lower() for i, c in enumerate(s))
+
+
+@app.route('/redirect_endpoint')
+def redirect_endpoint():
+    return redirect(url_for('nextpage'))
+
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555, debug=True)
+    
+# topics covered -> HTML Templates, Jinja2, Filters, Template Inheritance, Redirects, URL Building, Static Files, Error Handling, and more.
     
     
 # @app.route('/hello')
